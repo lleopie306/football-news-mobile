76768ce7358ea83101265f8c233dbf0c4aecad89
lleopie306 <desver4@gmail.com>
2025-11-19 20:23:15 +0700

    Benerin Tutorial 8

--- lib/widgets/news_card.dart
+++ lib/widgets/news_card.dart
@@ -1,68 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:football_news/screens/menu.dart' show ItemHomepage;
 import 'package:football_news/screens/newslist_form.dart';
 import 'package:football_news/screens/news_entry_list.dart';
 import 'package:football_news/screens/login.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 
+// Class data dari menu.dart
+class ItemHomepage {
+  final String name;
+  final IconData icon;
+
+  ItemHomepage(this.name, this.icon);
+}
+
+// Class ItemCard (dipindah dari menu.dart)
 class ItemCard extends StatelessWidget {
-  // Menampilkan kartu dengan ikon dan nama.
   final ItemHomepage item;
 
   const ItemCard(this.item, {super.key});
 
-  // Warna berbeda per tombol (optional: sesuaikan nama yang kamu pakai)
-  Color _backgroundFor(ItemHomepage item) {
-    switch (item.name) {
-      case 'All Products':
-        return Colors.blue;      // biru
-      case 'My Products':
-        return Colors.green;     // hijau
-      case 'Create Product':
-        return Colors.red;       // merah
-      default:
-        return Colors.blueAccent; // fallback
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
+
     return Material(
-      color: _backgroundFor(item),
+      color: Theme.of(context).colorScheme.secondary,
       borderRadius: BorderRadius.circular(12),
       child: InkWell(
         onTap: () async {
-          // Snackbar info
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
-            ..showSnackBar(
-              SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!")),
-            );
-
-          // Contoh navigasi jika tombol tertentu ditekan
-          if (item.name == 'Add News') {
+            ..showSnackBar(SnackBar(
+                content: Text("Kamu telah menekan tombol ${item.name}!")));
+          if (item.name == "Add News") {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (_) => const NewsFormPage()),
+              MaterialPageRoute(
+                builder: (context) => const NewsFormPage(),
+              ),
             );
           }
           else if (item.name == "See Football News") {
             Navigator.push(
               context,
               MaterialPageRoute(
-                  builder: (context) => const NewsEntryListPage()
+                builder: (context) => const NewsEntryListPage(),
               ),
             );
           }
           else if (item.name == "Logout") {
-            // TODO: Replace the URL with your app's URL and don't forget to add a trailing slash (/)!
-            // To connect Android emulator with Django on localhost, use URL http://10.0.2.2/
-            // If you using chrome,  use URL http://localhost:8000
-
-            final response = await request.logout(
-                "http://localhost:8000/auth/logout/");
+            // URL untuk emulator Android
+            const String url = "http://localhost:8000/auth/logout/";
+            final response = await request.logout(url);
             String message = response["message"];
             if (context.mounted) {
               if (response['status']) {
@@ -70,16 +59,15 @@ class ItemCard extends StatelessWidget {
                 ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                   content: Text("$message See you again, $uname."),
                 ));
+                // Kembali ke halaman Login
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => const LoginPage()),
                 );
               } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(message),
-                  ),
-                );
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text(message),
+                ));
               }
             }
           }
@@ -90,8 +78,12 @@ class ItemCard extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(item.icon, color: Colors.white, size: 30),
-                const SizedBox(height: 3),
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
                 Text(
                   item.name,
                   textAlign: TextAlign.center,
